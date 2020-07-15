c2ca46c076468cbbc05c08458a5717c38ebb6822
Intuinum <intuinum@outlook.com>
2020-07-15 02:57:26 -0400

    No longer uses the DynamicSection function.

--- src/index.js
+++ src/index.js
@@ -1,7 +1,18 @@
-import Navbar from './components/navbar/index.js';
-import DynamicSection from './components/dynamic/index';
+import Navbar from './components/navbar/index';
+import Home from './components/home/index';
 
 const root = document.getElementById('root');
+root.style.height = `${window.innerHeight}px`;
 
+const home = Home();
+
+console.log('Adding navbar');
 root.appendChild(Navbar('Home', 'Menu', 'About'));
-root.appendChild(DynamicSection());
+console.log('Adding home');
+root.appendChild(home);
+
+/*root.removeChild(root.childNodes[root.childNodes.length - 1]);
+console.log('Removed home');
+console.log('Adding menu');
+root.appendChild(menu);
+console.log('Menu added');*/
