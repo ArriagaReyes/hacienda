68ea5e9458d0e36cf8d20377e8525ab739b497d6
Intuinum <intuinum@outlook.com>
2020-07-15 03:00:08 -0400

    Updated the Home page styling. Can also use the "Our Menu" button to navagate to the Menu page.

--- src/components/home/index.js
+++ src/components/home/index.js
@@ -1,16 +1,26 @@
+import Element from '../element';
+import Menu from '../menu/index';
+
 export default function Home() {
-    const home = document.createElement('div');
-    const title = document.createElement('div');
-    const content = document.createElement('div');
-    home.id = 'Home';
-    home.className = 'home';
+    const home = Element('div', 'Home', 'section');
+    const title = Element('div', null, 'title');
+    const content = Element('div', null, 'sub-title');
+    const menuLink = Element('div', null, 'menu-link');
     title.textContent = 'Hacienda "La Margarita"';
     content.textContent = '5239 Main St South Fallsburg, NY';
+    menuLink.textContent = 'Our Menu';
+    menuLink.addEventListener(
+        'click',
+        e => {
+            const root = document.getElementById('root');
+            root.childNodes[root.childNodes.length - 1].replaceWith(Menu());
+            document.getElementById('page').textContent = 'Menu';
+        }
+    );
 
     home.appendChild(title);
     home.appendChild(content);
-
-
+    home.appendChild(menuLink);
 
     return home;
 }
