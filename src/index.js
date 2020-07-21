4aeb94adc8774a6b20c3587c1cff21d97287cf75
Intuinum <intuinum@outlook.com>
2020-07-20 23:24:13 -0400

    Added a resize feature.

--- src/index.js
+++ src/index.js
@@ -4,14 +4,20 @@ import Home from './components/home/index';
 const root = document.getElementById('root');
 root.style.height = `${window.innerHeight}px`;
 
+window.addEventListener(
+    'resize', 
+    e => {
+        root.style.height = `${window.innerHeight}px`;
+        console.log('Resized');
+    }
+);
+
 const home = Home();
 
 console.log('Adding navbar');
-root.appendChild(Navbar('Home', 'Menu', 'About'));
-console.log('Adding home');
-root.appendChild(home);
+root.append(Navbar());
+root.append(Home());
 
-console.log(root.replaceChild);
 
 /*root.removeChild(root.childNodes[root.childNodes.length - 1]);
 console.log('Removed home');
