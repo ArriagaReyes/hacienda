36e73f0622d13f694d4c7871f537d60163ce4da6
Intuinum <intuinum@outlook.com>
2020-07-20 23:32:11 -0400

    Made the home page css simpler to read and made some styling changes.

--- src/components/home/index.js
+++ src/components/home/index.js
@@ -3,25 +3,33 @@ import Menu from '../menu/index';
 import './index.scss';
 
 export default function Home() {
-    const home = Element('div', 'Home', 'section');
-    const title = Element('div', null, 'title');
-    const content = Element('div', null, 'sub-title');
-    const menuLink = Element('div', null, 'menu-link');
-    title.textContent = 'Hacienda "La Margarita"';
-    content.textContent = '5239 Main St South Fallsburg, NY';
+    const home = Element({type: 'section'});
+    const title = Element({type: 'h1'});
+    const content = Element({type: 'h2'});
+    const menuLink = Element({type: 'a'});
+    title.innerHTML = `
+        <div>Hacienda</div>
+        <div>"La</div>
+        <div>Margarita"</div>
+    `;
+    content.innerHTML = `
+        <div>5239 Main St South Fallsburg, NY</div>
+        <div>845-434-0416</div>
+    `;
+
     menuLink.textContent = 'Our Menu';
-    menuLink.addEventListener(
+    /*menuLink.addEventListener(
         'click',
         e => {
             const root = document.getElementById('root');
             root.childNodes[root.childNodes.length - 1].replaceWith(Menu());
             document.getElementById('page').textContent = 'Menu';
         }
-    );
+    );*/
 
     home.appendChild(title);
     home.appendChild(content);
-    home.appendChild(menuLink);
+    //home.appendChild(menuLink);
 
     return home;
 }
