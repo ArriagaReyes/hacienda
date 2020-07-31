b37ef85dcc6fffa287b27cd316ea9473256c53a0
Intuinum <intuinum@outlook.com>
2020-07-31 07:07:44 -0400

    Catagory takes a name and list and returns a element populated with item elements and a heading.

--- src/components/catagory/index.js
+++ src/components/catagory/index.js
@@ -1,12 +1,49 @@
 import Element from '../element';
+import './index.scss';
 
-export default function Catagory(name, list) {
-    const catagory = Element({type: 'section'});
-    const title = Element({type: 'div'});
-
-    title.textContent = name;
+export default function Catagory(name, list, callBack) {
+    const NAVBAR = document.getElementById('nav');
+    const CATAGORY = Element({type: 'section'});
+    const TITLE = Element({type: 'h1'});
+    CATAGORY.style.height = `calc(100% - ${NAVBAR.clientHeight}px)`;
+    TITLE.textContent = name;
     
-    catagory.appendChild(title);
+    CATAGORY.appendChild(TITLE);
+    CATAGORY.appendChild(List(list));
+    CATAGORY.appendChild(Close(callBack));
 
-    return catagory;
+    return CATAGORY;
+}
+
+function List(arr) {
+    const LIST = Element({type: 'ul', className: 'items'});
+
+    for (const item of arr) {
+        const ITEM = Element({type: 'li'});
+        const NAME = Element({type: 'div'});
+        const TEXT = Element({type: 'div'});
+
+        NAME.textContent = item.name;
+        TEXT.textContent = item.text;
+
+        ITEM.appendChild(NAME);
+        ITEM.appendChild(TEXT);
+
+        LIST.appendChild(ITEM);
+    }
+
+    return LIST;
+}
+
+function Close(callback) {
+    const BUTTON = Element({type: 'button'});
+    BUTTON.textContent = 'button';
+    BUTTON.addEventListener(
+        'click',
+        e => {
+            callback();
+        }
+    );
+
+    return BUTTON;
 }
